ea4d5de553da196a37e1dd43d56781ac9e469124
Ashish Khadka <aasheeshkhadka@gmail.com>
2025-05-19 18:01:58 +0545

    menu under development

--- front-page.php
+++ front-page.php
@@ -27,6 +27,7 @@ get_header();
     	<!-- top-slider section area --> 
 
 		<?php get_template_part( 'template-parts/special-section', 'none' ); ?>
+		<?php get_template_part( 'template-parts/menu-section', 'none' ); ?>
 	</main><!-- #main -->
 
 <?php
