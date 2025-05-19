5178d0afa6af107f38f3eb9b941e2fd34a4d3f82
swastik <swastik@kodiary.com>
2025-05-19 12:07:34 +0545

    frontpage

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is homepage template
+ *
+ * @package Maxims_Banquet
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<!-- top-slider section area -->
+    	<?php get_template_part( 'template-parts/slider', 'none' ); ?>
+    	<!-- top-slider section area --> 
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
